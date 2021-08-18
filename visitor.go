d4e215f346ebdc47cc2c5986bb2ac61f48a83347
Lei Wang <ssst0n3@gmail.com>
2021-08-18 23:39:58 +0800

    fix last stmt is InlineHtml error

--- visitor.go
+++ visitor.go
@@ -22,7 +22,7 @@ func (v *PhpTagFinder) EnterNode(w walker.Walkable) bool {
 	n := w.(node.Node)
 	switch typedNode := n.(type) {
 	case *node.Root:
-		log.Logger.Info("root")
+		log.Logger.Debug("root")
 		if len(typedNode.Stmts) > 0 {
 			firstStmt := typedNode.Stmts[0]
 			switch firstStmt.(type) {
@@ -33,14 +33,19 @@ func (v *PhpTagFinder) EnterNode(w walker.Walkable) bool {
 				tag := NewTag(position.NewPosition(1, fsPos.StartLine, 0, fsPos.StartPos))
 				v.Tags = append(v.Tags, tag)
 			}
+
+			lastStmt := typedNode.Stmts[len(typedNode.Stmts)-1]
+			if _, is := lastStmt.(*stmt.InlineHtml); is {
+				return true
+			}
 		}
 	case *stmt.InlineHtml:
-		log.Logger.Info("html")
+		log.Logger.Debug("html")
 		pos := n.GetPosition()
 		tag := NewTag(position.NewPosition(pos.EndLine, -1, pos.EndPos, -1))
 		v.Tags = append(v.Tags, tag)
 	default:
-		log.Logger.Info("default")
+		log.Logger.Debug("default")
 		if len(v.Tags) > 0 {
 			last := v.Tags[len(v.Tags)-1]
 			if last.Position.EndLine == -1 {
